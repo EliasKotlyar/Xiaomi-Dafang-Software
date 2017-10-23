2a802954184d64ffe680af17375b50077c516a44
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-10-23 23:32:10 +0200

    Dynamic Linking

--- compile.sh
+++ compile.sh
@@ -1,10 +1,12 @@
 export CC=mipsel-linux-gnu-gcc
-export CFLAGS="-DDEBUG_TRACE -DFAKE_ROOT -lcrypt"
-export LIBS="-lcrypt"
+export CFLAGS="-DDEBUG_TRACE -DFAKE_ROOT "
+export LIBS=""
+export LD=mipsel-linux-gnu-ld
+export LDFLAGS="-L./ -l:libcrypto.so"
 rm dropbearmulti
 cd dropbear-2017.75/
 make clean
 ./configure --host=mipsel-linux --disable-zlib
-make PROGRAMS="dropbear dbclient scp dropbearkey dropbearconvert" MULTI=1 STATIC=1 SCPPROGRESS=1
+make PROGRAMS="dropbear dbclient scp dropbearkey dropbearconvert" MULTI=1 SCPPROGRESS=1
 mv dropbearmulti ../dropbearmulti
 cd ..
