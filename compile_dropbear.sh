425ce4fb9404f38a393059637cce26661f26594a
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-11-01 17:54:38 +0100

    Added live555

--- compile_dropbear.sh
+++ compile_dropbear.sh
@@ -2,10 +2,8 @@
 TOOLCHAIN=$(pwd)/mips-gcc472-glibc216-64bit/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
 export CC=${CROSS_COMPILE}gcc
-export CFLAGS="-DDEBUG_TRACE -DFAKE_ROOT "
-export LIBS=""
 export LD=${CROSS_COMPILE}ld
-export CFLAGS="-muclibc -O2"
+export CFLAGS="-muclibc -O2 -DDEBUG_TRACE -DFAKE_ROOT "
 export CPPFLAGS="-muclibc -O2"
 export LDFLAGS="-muclibc -O2"
 
