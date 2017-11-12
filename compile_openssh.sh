03fac1a82f23ccf48976f2d422022fb3426ff886
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-11-12 01:51:47 +0100

    New openssl-binary

--- compile_openssh.sh
+++ compile_openssh.sh
@@ -8,6 +8,11 @@ echo "Using the following OPENSSL: $OPENSSL_DIR"
 echo "Using the following ZLIB: $ZLIB_DIR"
 cd openssh
 make clean
+export CC=${CROSS_COMPILE}gcc
+export LD=${CROSS_COMPILE}ld
+export CFLAGS="-muclibc -O2"
+export CPPFLAGS="-muclibc -O2"
+export LDFLAGS="-muclibc -O2"
 
 
 ./configure  --with-cflags="-muclibc"  --with-cppflags="-muclibc -I${OPENSSL_HEADER_DIR}"  --with-ldflags="-muclibc " --prefix=${PWD}/_install --host=mips-linux --with-libs --with-zlib=${ZLIB_DIR} --with-ssl-dir=${OPENSSL_DIR} --disable-etc-default-login CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar
