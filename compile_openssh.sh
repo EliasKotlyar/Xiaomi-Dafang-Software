09c7f9482413d98c0a8d04d7bbb32adc8ec632a6
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-11-12 01:18:02 +0100

    Header for libc

--- compile_openssh.sh
+++ compile_openssh.sh
@@ -1,6 +1,7 @@
 TOOLCHAIN=$(pwd)/mips-gcc472-glibc216-64bit/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
 OPENSSL_DIR=$(pwd)/openssl/_install
+OPENSSL_HEADER_DIR=$(pwd)/openssl/_install/include/
 ZLIB_DIR=$(pwd)/zlib/_install/
 echo "Using the following Toolchain $TOOLCHAIN"
 echo "Using the following OPENSSL: $OPENSSL_DIR"
@@ -9,7 +10,7 @@ cd openssh
 make clean
 
 
-./configure  --with-cflags="-muclibc"  --with-cppflags="-muclibc "  --with-ldflags="-muclibc " --prefix=${PWD}/_install --host=mips-linux --with-libs --with-zlib=${ZLIB_DIR} --with-ssl-dir=${OPENSSL_DIR} --disable-etc-default-login CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar
+./configure  --with-cflags="-muclibc"  --with-cppflags="-muclibc -I${OPENSSL_HEADER_DIR}"  --with-ldflags="-muclibc " --prefix=${PWD}/_install --host=mips-linux --with-libs --with-zlib=${ZLIB_DIR} --with-ssl-dir=${OPENSSL_DIR} --disable-etc-default-login CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar
 ## Remove Strip here
 #sed '/STRIP_OPT=-s/d' ./Makefile
 
