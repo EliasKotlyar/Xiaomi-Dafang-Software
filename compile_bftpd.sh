064970f585a464a7a74d0275e8752a31299a297f
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-11-19 10:27:34 +0100

    Btftp

--- compile_bftpd.sh
+++ compile_bftpd.sh
@@ -3,12 +3,12 @@ TOOLCHAIN=$(pwd)/mips-gcc472-glibc216-64bit/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
 export CC=${CROSS_COMPILE}gcc
 export LD=${CROSS_COMPILE}ld
-export CFLAGS="-muclibc -O2 -DDEBUG_TRACE -DFAKE_ROOT "
+export CFLAGS=""
 export CPPFLAGS="-muclibc -O2"
 export LDFLAGS="-muclibc -O2"
 
 rm bftpd
 cd bftpd/
 make clean
-./configure --host=mips-linux
+./configure --host=mips-linux --enable-debug
 make
