565b026a366c7e4f1c9e9ab89011a8a872cb1aef
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-11-26 13:59:41 +0100

    compile of h264

--- videostreamer/compile_upload_encoder.sh
+++ videostreamer/compile_upload_encoder.sh
@@ -1,3 +1,4 @@
 ./compile.sh
 HOST=192.168.0.99
-ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard testH264VideoStreamer
+ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard mJpegStreamer
+ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard h264streamer
