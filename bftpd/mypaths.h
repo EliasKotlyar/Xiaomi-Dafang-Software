fb179d5930724a1643cb9a1022673959a5e4a657
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-12-03 20:35:52 +0100

    mypaths

--- bftpd/mypaths.h
+++ bftpd/mypaths.h
@@ -1,4 +1,4 @@
-#define PATH_BFTPD_CONF "/system/dafang/bftpd.conf"
+#define PATH_BFTPD_CONF "/system/sdcard/config/bftpd.conf"
 #ifdef PREFIX
 #define PATH_BFTPD_CONF_WITH_PREFIX PREFIX"/system/dafang/bftpd.conf"
 #endif
