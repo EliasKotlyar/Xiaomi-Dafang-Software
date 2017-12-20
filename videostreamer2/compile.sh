a205904402324ac5c3a839639786dcb9357e1108
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-12-20 20:04:26 +0100

    videostreamer2

--- videostreamer2/compile.sh
+++ videostreamer2/compile.sh
@@ -4,7 +4,7 @@ CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
 export CROSS_COMPILE=${CROSS_COMPILE}
 export CC=${CROSS_COMPILE}gcc
 export LD=${CROSS_COMPILE}ld
-export CFLAGS="-muclibc -O2 -DDEBUG_TRACE -DFAKE_ROOT "
+export CFLAGS="-muclibc -O2 "
 export CPPFLAGS="-muclibc -O2"
 export LDFLAGS="-muclibc -O2"
 make clean
