ea87eaa47a289bd3a5691af3c9474ea54dec56c0
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-12-21 21:32:02 +0100

    ImpStuff

--- v4l2rtspserver-master/inc/ImpJpegDeviceSource.h
+++ v4l2rtspserver-master/inc/ImpJpegDeviceSource.h
@@ -2,11 +2,12 @@
 #define _WEBCAM_JPEG_DEVICE_SOURCE_HH
 
 #include "DeviceSource.h"
-
+#include "logger.h"
 class ImpJpegDeviceSource : public V4L2DeviceSource {
 public:
 
     static ImpJpegDeviceSource* createNew(UsageEnvironment& env, DeviceInterface * device, int outputFd, unsigned int queueSize, bool useThread) ;
+    int getNextFrame();
 
 };
 
--- v4l2rtspserver-master/src/ImpJpegDeviceSource.cpp
+++ v4l2rtspserver-master/src/ImpJpegDeviceSource.cpp
@@ -1,5 +1,40 @@
 #include "ImpJpegDeviceSource.h"
 
 ImpJpegDeviceSource* ImpJpegDeviceSource::createNew(UsageEnvironment& env, DeviceInterface * device, int outputFd, unsigned int queueSize, bool useThread){
-    return NULL;
+    return new ImpJpegVideoDeviceSource(env, params);
 }
+
+int ImpJpegDeviceSource::getNextFrame()
+{
+    timeval ref;
+    gettimeofday(&ref, NULL);
+    //char buffer[m_device->getBufferSize()];
+    int frameSize = impEncoder->snap_jpeg();
+
+    //int frameSize = m_device->read(buffer,  m_device->getBufferSize());
+    if (frameSize < 0)
+    {
+        LOG(NOTICE) << "V4L2DeviceSource::getNextFrame errno:" << errno << " "  << strerror(errno);
+    }
+    else if (frameSize == 0)
+    {
+        LOG(NOTICE) << "V4L2DeviceSource::getNextFrame no data errno:" << errno << " "  << strerror(errno);
+    }
+    else
+    {
+        timeval tv;
+        gettimeofday(&tv, NULL);
+        timeval diff;
+        timersub(&tv,&ref,&diff);
+        m_in.notify(tv.tv_sec, frameSize);
+        LOG(DEBUG) << "getNextFrame\ttimestamp:" << ref.tv_sec << "." << ref.tv_usec << "\tsize:" << frameSize <<"\tdiff:" <<  (diff.tv_sec*1000+diff.tv_usec/1000) << "ms";
+        processFrame(impEncoder->getBuffer(),frameSize,ref);
+        if (m_outfd != -1)
+        {
+            write(m_outfd, impEncoder->getBuffer(), frameSize);
+        }
+    }
+    return frameSize;
+}
+
+
