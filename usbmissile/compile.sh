5570a5191b2a686c66f347764c0160e7983fb1f4
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-02-17 13:37:36 +0100

    Compile

--- usbmissile/compile.sh
+++ usbmissile/compile.sh
@@ -6,9 +6,23 @@ export CC=${CROSS_COMPILE}gcc
 export CPLUSPLUS=${CROSS_COMPILE}gcc
 export LD=${CROSS_COMPILE}ld
 
-LIBUSB_PATH=${PWD}/../libusb-compat-0.1.5/_install/
-export CFLAGS="-muclibc -O2 -I$LIBUSB_PATH/include/ -c -O2 -Wall"
+
+LIBUSB_PATH=${PWD}/../libusb-1.0.21/_install/
+#CFLAGS=-I$LIBUSB_PATH/include/libusb-1.0
+
+
+LIBCOMPAT_PATH=${PWD}/../libusb-compat-0.1.5/_install
+CFLAGS=-I$LIBCOMPAT_PATH/include/
+#LDFLAG=-L$LIBUSB_PATH/lib $LDFLAG
+
+LDFLAG="-lusb-1.0 -L$LIBUSB_PATH/lib -lusb -L$LIBCOMPAT_PATH/lib"
+
+export CFLAGS="-muclibc -O2  -c -O2 -Wall $CFLAGS"
 export CPPFLAGS="-muclibc -O2"
-export LDFLAGS="-muclibc -O2 -L$LIBUSB_PATH/lib -lusb"
+export LDFLAGS="-muclibc -O2 $LDFLAG"
+
+echo $CFLAGS
+echo $LDFLAGS
+
 make clean
 make
