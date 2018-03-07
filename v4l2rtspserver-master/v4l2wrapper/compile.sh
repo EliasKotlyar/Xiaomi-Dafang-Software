009b22f1bceb84f8201c5281c9262a56d97f5e64
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-03-07 23:18:54 +0100

    V4lwrapper

--- v4l2rtspserver-master/v4l2wrapper/compile.sh
+++ v4l2rtspserver-master/v4l2wrapper/compile.sh
@@ -4,6 +4,7 @@ CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
 export CROSS_COMPILE=${CROSS_COMPILE}
 export CC=${CROSS_COMPILE}gcc
 export LD=${CROSS_COMPILE}ld
+export AR=${CROSS_COMPILE}ar
 export CFLAGS="-muclibc -O2 "
 export CPPFLAGS="-muclibc -O2"
 export LDFLAGS="-muclibc -O2"
