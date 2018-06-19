0d953e73b5c99ce94f865111e53d275352bb44a2
Dave Walker (Daviey) <email@daviey.com>
2018-06-19 08:53:45 +0100

    Busybox: Compile flash_eraseall
    The standard busybox config doesn't include the applet flash_eraseall,
    which is needed for erasing the flash device.
    
    This is included in the stock busybox, but not in the custom one.
    
    Closes: EliasKotlyar/Xiaomi-Dafang-Hacks#496
    
    Signed-off-by: Dave Walker (Daviey) <email@daviey.com>

--- compile_busybox.sh
+++ compile_busybox.sh
@@ -20,4 +20,8 @@ sed -e 's/CONFIG_FALLOCATE=y/CONFIG_FALLOCATE=n/' -i .config
 sed -e 's/CONFIG_NSENTER=y/CONFIG_NSENTER=n/' -i .config
 sed -e 's/CONFIG_FSYNC=y/CONFIG_FSYNC=n/' -i .config
 sed -e 's/CONFIG_SYNC=y/CONFIG_SYNC=n/' -i .config
+
+# Enable some features (note the /c which changes the entire line)
+sed -e '/CONFIG_FLASH_ERASEALL/c\CONFIG_FLASH_ERASEALL=y' -i .config
+
 make CROSS_COMPILE=$CROSS_COMPILE
